99ba1b7fdf01ec0be5f4d423ff4f1a05402d33ad
HTTPimp <theking@httpimp.com>
2017-10-03 00:48:11 -0700

    Cleaning up monitor docs

--- monitor_func.go
+++ monitor_func.go
@@ -6,7 +6,7 @@ import (
 )
 
 // MonitorFunc turns a function into a Monitor
-func MonitorFunc(interval time.Duration, logFunc func(Stats)) *monitorFunc {
+func MonitorFunc(interval time.Duration, logFunc func(Stats)) Monitor {
 	return &monitorFunc{
 		interval: interval,
 		logFunc:  logFunc,
