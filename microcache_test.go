84727fe61772816fd6dc43825e4989a195f5bb7c
HTTPimp <theking@httpimp.com>
2017-10-16 23:41:35 -0700

    Adding test for collapsed forwarding

--- microcache_test.go
+++ microcache_test.go
@@ -3,6 +3,7 @@ package microcache
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 	"time"
 )
@@ -37,10 +38,10 @@ func TestTTL(t *testing.T) {
 func TestHashQuery(t *testing.T) {
 	testMonitor := &monitorFunc{interval: 100 * time.Second, logFunc: func(Stats) {}}
 	cache := New(Config{
-		TTL:         30 * time.Second,
-		HashQuery:   true,
-		Monitor:     testMonitor,
-		Driver:      NewDriverLRU(10),
+		TTL:       30 * time.Second,
+		HashQuery: true,
+		Monitor:   testMonitor,
+		Driver:    NewDriverLRU(10),
 	})
 	handler := cache.Middleware(http.HandlerFunc(noopSuccessHandler))
 	batchGet(handler, []string{
@@ -58,10 +59,10 @@ func TestHashQuery(t *testing.T) {
 func TestHashQueryDisabled(t *testing.T) {
 	testMonitor := &monitorFunc{interval: 100 * time.Second, logFunc: func(Stats) {}}
 	cache := New(Config{
-		TTL:         30 * time.Second,
-		HashQuery:   false,
-		Monitor:     testMonitor,
-		Driver:      NewDriverLRU(10),
+		TTL:       30 * time.Second,
+		HashQuery: false,
+		Monitor:   testMonitor,
+		Driver:    NewDriverLRU(10),
 	})
 	handler := cache.Middleware(http.HandlerFunc(noopSuccessHandler))
 	batchGet(handler, []string{
@@ -270,6 +271,32 @@ func TestTimeout(t *testing.T) {
 	cache.Stop()
 }
 
+// CollapsedFowarding
+func TestCollapsedFowarding(t *testing.T) {
+	testMonitor := &monitorFunc{interval: 100 * time.Second, logFunc: func(Stats) {}}
+	cache := New(Config{
+		TTL:                 30 * time.Second,
+		CollapsedForwarding: true,
+		Monitor:             testMonitor,
+		Driver:              NewDriverLRU(10),
+	})
+	handler := cache.Middleware(http.HandlerFunc(timelySuccessHandler))
+	start := time.Now()
+	parallelGet(handler, []string{
+		"/",
+		"/",
+		"/",
+		"/",
+		"/",
+		"/",
+	})
+	if testMonitor.misses != 1 || testMonitor.hits != 5 || time.Since(start) > 20*time.Millisecond {
+		t.Log("CollapsedFowarding not respected - got", testMonitor.hits, "hits")
+		t.Fail()
+	}
+	cache.Stop()
+}
+
 // --- helper funcs ---
 
 func batchGet(handler http.Handler, urls []string) {
@@ -277,6 +304,19 @@ func batchGet(handler http.Handler, urls []string) {
 		r1, _ := http.NewRequest("GET", url, nil)
 		handler.ServeHTTP(httptest.NewRecorder(), r1)
 	}
+}
+
+func parallelGet(handler http.Handler, urls []string) {
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		r1, _ := http.NewRequest("GET", url, nil)
+		wg.Add(1)
+		go func() {
+			handler.ServeHTTP(httptest.NewRecorder(), r1)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 func noopSuccessHandler(w http.ResponseWriter, r *http.Request) {
@@ -294,5 +334,10 @@ func failureHandler(w http.ResponseWriter, r *http.Request) {
 
 func slowSuccessHandler(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(100 * time.Millisecond)
+	http.Error(w, "done", 200)
+}
+
+func timelySuccessHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(10 * time.Millisecond)
 	http.Error(w, "done", 200)
 }
