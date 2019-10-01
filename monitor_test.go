79c3bdd80649a97f3e6875b913ad03d6daf90cba
Kev Burns <kevburnsjr@gmail.com>
2019-10-01 00:13:47 -0700

    Add test for microcache monitor call

--- monitor_test.go
+++ monitor_test.go
@@ -1,6 +1,7 @@
 package microcache
 
 import (
+	"net/http"
 	"testing"
 	"time"
 )
@@ -16,5 +17,25 @@ func TestMonitor(t *testing.T) {
 	testMonitor.Log(Stats{})
 	if hits != expected {
 		t.Fatalf("Monitor not logging correctly (%d != %d)", hits, expected)
+	}
+}
+
+// Microcache calls monitor
+func TestMicrocacheCallsMonitor(t *testing.T) {
+	var statChan = make(chan int)
+	testMonitor := &monitorFunc{interval: 10 * time.Millisecond, logFunc: func(s Stats) {
+		statChan <- s.Size
+	}}
+	cache := New(Config{
+		TTL:     30 * time.Second,
+		Monitor: testMonitor,
+		Driver:  NewDriverLRU(10),
+	})
+	defer cache.Stop()
+	handler := cache.Middleware(http.HandlerFunc(noopSuccessHandler))
+	batchGet(handler, []string{"/"})
+	size := <- statChan
+	if size != 1 {
+		t.Fatal("Monitor was not called by microcache")
 	}
 }
