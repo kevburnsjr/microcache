d55648159b369abca2046492f75287e63f21d89d
Kev Burns <kevburnsjr@gmail.com>
2019-10-02 09:24:58 -0700

    Replace http.Error with w.Write in benchmark handlers

--- microcache_bench_test.go
+++ microcache_bench_test.go
@@ -13,7 +13,7 @@ func BenchmarkHits(b *testing.B) {
 		Driver: NewDriverLRU(10),
 	})
 	defer cache.Stop()
-	handler := cache.Middleware(http.HandlerFunc(noopSuccessHandler))
+	handler := cache.Middleware(http.HandlerFunc(successHandler))
 	r, _ := http.NewRequest("GET", "/", nil)
 	w := &noopWriter{http.Header{}}
 	b.ResetTimer()
@@ -28,7 +28,7 @@ func BenchmarkNocache(b *testing.B) {
 		Driver:  NewDriverLRU(10),
 	})
 	defer cache.Stop()
-	handler := cache.Middleware(http.HandlerFunc(noopSuccessHandler))
+	handler := cache.Middleware(http.HandlerFunc(successHandler))
 	r, _ := http.NewRequest("GET", "/", nil)
 	w := &noopWriter{http.Header{}}
 	b.ResetTimer()
@@ -43,7 +43,7 @@ func BenchmarkMisses(b *testing.B) {
 		Driver: NewDriverLRU(10),
 	})
 	defer cache.Stop()
-	handler := cache.Middleware(http.HandlerFunc(noopSuccessHandler))
+	handler := cache.Middleware(http.HandlerFunc(successHandler))
 	r, _ := http.NewRequest("GET", "/", nil)
 	w := &noopWriter{http.Header{}}
 	b.ResetTimer()
@@ -116,8 +116,10 @@ func (w *noopWriter) Header() http.Header {
 
 func (w *noopWriter) WriteHeader(code int) {}
 
-var json1k = `{"counts":[{"bucket":1569888000,"total":115,"unique":39},{"bucket":1569801600,"total":150,"unique":38},{"bucket":1569715200,"total":129,"unique":34},{"bucket":1569628800,"total":142,"unique":34},{"bucket":1569542400,"total":151,"unique":39},{"bucket":1569456000,"total":145,"unique":44},{"bucket":1569369600,"total":143,"unique":49},{"bucket":1569283200,"total":174,"unique":46},{"bucket":1569196800,"total":407,"unique":52},{"bucket":1569110400,"total":357,"unique":51},{"bucket":1569024000,"total":227,"unique":44},{"bucket":1568937600,"total":257,"unique":44},{"bucket":1568851200,"total":238,"unique":47},{"bucket":1568764800,"total":246,"unique":62}],"summary":{"total":2881,"unique":108}}`
+func successHandler(w http.ResponseWriter, r *http.Request) {}
+
+var json1k = []byte(`{"counts":[{"bucket":1569888000,"total":115,"unique":39},{"bucket":1569801600,"total":150,"unique":38},{"bucket":1569715200,"total":129,"unique":34},{"bucket":1569628800,"total":142,"unique":34},{"bucket":1569542400,"total":151,"unique":39},{"bucket":1569456000,"total":145,"unique":44},{"bucket":1569369600,"total":143,"unique":49},{"bucket":1569283200,"total":174,"unique":46},{"bucket":1569196800,"total":407,"unique":52},{"bucket":1569110400,"total":357,"unique":51},{"bucket":1569024000,"total":227,"unique":44},{"bucket":1568937600,"total":257,"unique":44},{"bucket":1568851200,"total":238,"unique":47},{"bucket":1568764800,"total":246,"unique":62}],"summary":{"total":2881,"unique":108}}`)
 
 func success1kHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, json1k, 200)
+	w.Write(json1k)
 }
